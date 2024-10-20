d391be4a686b3c892ad90f60b2b6e7c74c07128c
Alexandr Kurganov <alexandr.kurs.it@gmail.com>
2024-10-20 23:47:23 +0600

    Install react-router-dom, create containers and components Home, About, Contacts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import Home from './containers/home/home';
+import About from './containers/about/about';
+import Contact from './containers/contacts/contacts';
 
+const App: React.FC = () => {
   return (
-    <>
-      Hello!
-    </>
-  )
-
+    <Router>
+      <nav>
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/about">About us</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 };
-export default App; 
+
+export default App;
